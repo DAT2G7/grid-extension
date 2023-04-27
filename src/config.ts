465721d0d0db530160ff5aca95126d065492c8be
bliztle <asbjoern.r.e@gmail.com>
2023-04-27 14:09:23 +0200

    .env support

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const GRID_SERVER_URL =
+    process.env.GRID_SERVER_URL ?? "https://grid.hauptmann.tech";
+
+export default {
+    GRID_SERVER_URL
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,29 @@
+const Dotenv = require("dotenv-webpack");
 const path = require("path");
 
-module.exports = {
-    // entry: "./src/index.ts",
-    entry: {
-        content: ["./src/content/index.ts"],
-        background: ["./src/background/index.ts"]
-    },
-    devtool: "inline-source-map",
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"]
-    },
-    output: {
-        filename: "[name].js",
-        path: path.resolve(__dirname, "dist")
-    }
+module.exports = () => {
+    return {
+        plugins: [new Dotenv()],
+        entry: {
+            content: ["./src/content/index.ts"],
+            background: ["./src/background/index.ts"]
+        },
+        devtool: "inline-source-map",
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: "ts-loader",
+                    exclude: /node_modules/
+                }
+            ]
+        },
+        resolve: {
+            extensions: [".tsx", ".ts", ".js"]
+        },
+        output: {
+            filename: "[name].js",
+            path: path.resolve(__dirname, "dist")
+        }
+    };
 };
