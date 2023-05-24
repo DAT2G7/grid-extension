8c9f006e5f700cc2cf3bf4a8d1bdfd6c63255528
bliztle <asbjoern.r.e@gmail.com>
2023-05-24 14:49:15 +0200

    comments

--- src/background/index.ts
+++ src/background/index.ts
@@ -2,14 +2,17 @@ import { navigateToGrid, updateGridTabIds } from "../common";
 
 import browser from "webextension-polyfill";
 
+// Open the grid when the browser starts
 browser.runtime.onStartup.addListener(() => {
     navigateToGrid(true);
 });
 
+// Open the grid when the extension is installed
 browser.runtime.onInstalled.addListener(() => {
     navigateToGrid();
 });
 
+// Update the grid tab ids when a tab is closed
 browser.tabs.onRemoved.addListener((tabId) => {
     updateGridTabIds((ids) => ids.filter((id) => id !== tabId));
 });
--- src/common.ts
+++ src/common.ts
@@ -6,6 +6,7 @@ export enum StorageKeyLocal {
     GridTabs = "grid-tabs"
 }
 
+/** Get ids for tracked tabs from browser storage */
 export const getGridTabIds = async (): Promise<number[]> => {
     const data = await browser.storage.local.get({
         [StorageKeyLocal.GridTabs]: []
@@ -13,6 +14,10 @@ export const getGridTabIds = async (): Promise<number[]> => {
     return data[StorageKeyLocal.GridTabs];
 };
 
+/**
+ * Set ids for tracked tabs in browser storage.
+ * Overwrites any existing data
+ */
 export const setGridTabIds = async (tabIds: number[]) => {
     await browser.storage.local.set({
         [StorageKeyLocal.GridTabs]: [tabIds]
@@ -31,14 +36,17 @@ export const updateGridTabIds = async (
     if (!_.isEqual(ids, newIds)) await setGridTabIds(newIds);
 };
 
+/** Clear all data from browser storage */
 export const clearStorage = async () => {
     await browser.storage.local.clear();
 };
 
+/** Open grid in new tab */
 export const navigateToGrid = async (quiet = false) => {
     const url = new URL(GRID_SERVER_URL + "/client");
     url.searchParams.set("quiet", quiet.toString());
 
+    // Open new tab and get already tracked tab ids
     const [tab, keys] = await Promise.all([
         browser.tabs.create({
             url: url.toString()
@@ -48,5 +56,6 @@ export const navigateToGrid = async (quiet = false) => {
 
     if (!tab.id) return;
 
+    // Update ids if tab was created successfully
     setGridTabIds([...keys, tab.id]);
 };
